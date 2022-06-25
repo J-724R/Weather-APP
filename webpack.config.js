18814bb2e52554a52a58d550c7d0a4a72c8d7bc8
J-724 <jjpernian@gmail.com>
2022-06-25 18:22:10 -0400

    Basic Setup created

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+        {
+          test: /\.js$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          }
+        },
+        {
+         test: /\.css$/i,
+        //  use: [MiniCssExtractPlugin.loader, 'css-loader'],
+         use:[
+           {
+             loader: MiniCssExtractPlugin.loader,
+             options: {
+               publicPath: "./",
+             },
+           },
+          //  "style-loader",
+           "css-loader",
+         ]
+        },
+        {
+         test: /\.html$/i,
+         use: [
+           {
+           loader: 'html-loader',
+           options: {
+             minimize:true,
+           },
+           }
+          ],
+        },
+        {
+         test: /\.(svg|jpg|jpeg|gif|png)$/i,
+         type: 'asset/resource',
+        },
+        {
+         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+         type: 'asset/resource',
+        }, 
+    ]  
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:"./src/template.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+}; 
