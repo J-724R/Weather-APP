1e9bb4452b2ed8f76d15a4e7cb5e601ace2604b0
J-724 <jjpernian@gmail.com>
2022-06-29 17:20:15 -0400

    Half day 3

--- src/index.js
+++ src/index.js
@@ -1,65 +1,41 @@
-import layout from './layout.css';
-import style from './style.css';
+import './main.css';
+// import API from './components/weatherAPI.js';
 
-
-// - Your API key is 85051ca8511a9148395ef7966174f9c7
-
-// getWeatherAsync();
 console.log(`\n\n\n`);
 
-function printCoords(data){
-    console.log(" ");
-    console.log(data);
-    console.log(`
-        country: ${data[0].country} \n
-        state: ${data[0].state} \n
-        name: ${data[0].name} \n 
-        latitude: ${data[0].lat} \n 
-        longitude: ${data[0].lon}`);
-    
-}
 
-function printData(data){
-    console.log(" ");
-    console.log(data);
-    console.log(`
-        country: ${data[0].country} \n
-        state: ${data[0].state} \n
-        name: ${data[0].name} \n 
-        latitude: ${data[0].lat} \n 
-        longitude: ${data[0].lon}`);
+function procesData(data){
+    const _safedata = data
+        .replace(',', '')
+        .replace(/\/|<|>/g, '')
+        .split(' ');
     
-}
-
-async function getWeather (location, country){
-    const Key = "85051ca8511a9148395ef7966174f9c7";
-    let unit = "metric";
-    //get metrics
-    if (!country) {
-        country = "";
+    if((_safedata.length > 2)
+        || (_safedata[1].length > 2)){
+        return console.log("Please match the pattern")
     }
 
-    try {
-        const requestCoords =  await 
-            fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location},${country}&limit=1&appid=${Key}`,
-            { method: "GET" }
-        );
-        const coords = await requestCoords.json();
-        printCoords(coords);
-
-
-        const requestWeather = await
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coords[0].lat}&lon=${coords[0].lon}&exclude=minutely&units=${unit}&appid=${Key}`,
-            { method: "GET" }
-        );
-        const weather = await requestWeather.json();
-        printData(weather);
-                 
-
-
-    }catch(error){
-        return console.log(error);
+    const dataobj = {
+        city: _safedata[0],
+        country: _safedata[1]
     }
+  
+    return dataobj;
 }
 
-getWeather('cucuta', 'co');
+const searchBox = document.querySelector('#searchbox');
+const searchButton = document.querySelector('.searchbutton');
+
+searchBox.addEventListener('keydown', (e) => {
+    if (e.key === 'enter') searchButton.click();
+});
+
+searchButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const searchBoxValue = searchBox.value;
+    const safedata = procesData(searchBoxValue); 
+    console.log(safedata);
+    // API.getWeatherData(searchBoxValue);
+});
+
+//fucntion processData
