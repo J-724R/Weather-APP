e354cb2f681469c5261d978709898f126c003168
J-724 <jjpernian@gmail.com>
2022-07-03 01:15:56 -0400

    Day 6

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { weather, UI } from './components/eventHandlers.js';
 import _ from 'lodash';
 
 weather.eventHandlers();
-
+UI.changeForecast();
+UI.toggleForecast();
 
 console.log(`\n\n\n`);
 
